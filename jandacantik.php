b5c64698d3d30c168250a58655b11de58a0e33df
pecintajandamuda <66892372+pecintajandamuda@users.noreply.github.com>
2020-07-11 08:09:42 +0700

    Update and rename gopud.php to jandacantik.php

--- jandacantik.php
+++ jandacantik.php
@@ -1,6 +1,6 @@
 <?php
 date_default_timezone_set('Asia/Semarang');
-include "function.php";
+include "janda.php";
 echo color("red","SCRIPT GRATIS TIDAK DI JUAL \n");
 echo color("yellow"," Time  : ".date('[d-m-Y] [H:i:s]')."   \n");
 echo color("yellow"," khusus project ojo dishare  \n");
